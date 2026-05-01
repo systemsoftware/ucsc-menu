704ebbb9001a6261da416cfbcd35a643fee5ef9a
Bryce <bryce.a23@outlook.com>
2026-05-01 11:09:56 -0700

    Initial Commit

---
+++ UCSC Menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UCSC Menu
+//
+//  Created by Bryce Agostini on 5/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UCSC Menu/UCSC_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCSC_MenuApp.swift
+//  UCSC Menu
+//
+//  Created by Bryce Agostini on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct UCSC_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
